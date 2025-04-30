0d9586da604689a41e6406e08040a0a102d9e8f4
karthick-gsquare <karthick.v@gsquarehousing.com>
2025-04-30 15:51:00 +0530

    test-calling-feature

--- src/components/ui/cards/PhoneCard.jsx
+++ src/components/ui/cards/PhoneCard.jsx
@@ -1,9 +1,23 @@
-import {View, Text, Button} from 'react-native';
+import {View, Text,Linking,Alert} from 'react-native';
 import React from 'react';
 import styles from '../../../../StyleSheet';
 import MyButton from '../buttons/MyButton';
 
+const phoneNumber = '9150078249';
 const PhoneCard = () => {
+   const handleCall = async () => {
+      console.log('calling.....');
+  
+      const url = `tel:${phoneNumber}`;
+      const supported = await Linking.canOpenURL(url);
+      console.log(supported);
+  
+      if (supported) {
+        Linking.openURL(url);
+      } else {
+        Alert.alert('Error', 'Phone call not supported on this device');
+      }
+    };
   return (
     <View style={styles.phoneCardContainer}>
       <View style={styles.flex}>
@@ -11,7 +25,7 @@ const PhoneCard = () => {
         <Text style={{fontSize: 16, fontWeight: 900}}>91+ 9176148008 </Text>
       </View>
       <View style={{marginTop: 16}}>
-        <MyButton title={'Call Now'} />
+        <MyButton title={'Call Now'} onPress={handleCall}/>
       </View>
     </View>
   );
--- src/screens/home/HomeScreen.jsx
+++ src/screens/home/HomeScreen.jsx
@@ -1,4 +1,4 @@
-import {View, Text} from 'react-native';
+import {View, Text, Button, Linking, Alert} from 'react-native';
 import React from 'react';
 import styles from '../../../StyleSheet';
 import Logo from '../../components/common/logo/Logo';
@@ -11,8 +11,35 @@ const HomeScreen = () => {
       <Text style={styles.title}>Call History</Text>
       <View style={{marginTop: 24}}></View>
       <PhoneCard />
+      <View style={{marginTop:10}}>
+        <CallButton />
+      </View>
     </View>
   );
 };
 
 export default HomeScreen;
+
+const phoneNumber = '9962198009';
+
+const CallButton = () => {
+  const handleCall = async () => {
+    console.log('calling.....');
+
+    const url = `tel:${phoneNumber}`;
+    const supported = await Linking.canOpenURL(url);
+    console.log(supported);
+
+    if (supported) {
+      Linking.openURL(url);
+    } else {
+      Alert.alert('Error', 'Phone call not supported on this device');
+    }
+  };
+
+  return (
+    <View>
+      <Button title="Call Now" onPress={handleCall} />
+    </View>
+  );
+};
